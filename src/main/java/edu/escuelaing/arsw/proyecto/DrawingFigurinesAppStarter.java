46038ec977ded134fa41c147365e894e0dee6ed2
ronis97 <ronivillarreal@hotmail.com>
2022-06-29 18:43:44 -0500

    heroku deploy

--- src/main/java/edu/escuelaing/arsw/proyecto/DrawingFigurinesAppStarter.java
+++ src/main/java/edu/escuelaing/arsw/proyecto/DrawingFigurinesAppStarter.java
@@ -1,5 +1,23 @@
 package edu.escuelaing.arsw.proyecto;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Collections;
+
+@SpringBootApplication
 public class DrawingFigurinesAppStarter {
 
+    public static void main(String[] args){
+        SpringApplication app = new SpringApplication(DrawingFigurinesAppStarter.class);
+        app.setDefaultProperties(Collections
+                .singletonMap("server.port", getPort()));
+        app.run(args);
+    }
+    static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 8080; //returns default port if heroku-port isn't set (i.e. on localhost)
+    }
 }
